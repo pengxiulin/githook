8f263499f5c2201ea9d354d63cba8af0b5aa08ef
pengxiulin <pengxiulin@live.com>
2013-05-04 14:44:26 +0800

    modify function

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 var http = require('http');
 http.createServer(function (req, res) {
     var info = '';
-    req.addListener('data',function (chunk) {
+    req.on('data',function (chunk) {
         info += chunk;
-    }).addListener('end', function () {
-        console.log(info);
+    }).on('end', function () {
+        console.log(decodeURIComponent(info));
     });
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
